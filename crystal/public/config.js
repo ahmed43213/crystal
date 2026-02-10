61c41ca297ad4ac9c9408ff4bc6d7dd2cb6b0c5d
ahmed43213 <abnrim6@gmail.com>
2026-02-10 23:16:57 +0100

    Add files via upload

---
+++ crystal/public/config.js
@@ -0,0 +1,13 @@
+// Paste your Stripe Payment Links here.
+// How to get them:
+// Stripe Dashboard -> Payment links -> + New
+// After creating the link, set the "After payment" redirect to:
+//   https://YOUR_DOMAIN/success.html
+
+window.STRIPE_PAYMENT_LINKS = {
+  p1: "https://buy.stripe.com/dRmeVfb5S2ys7713Cb8bS0m",
+  p2: "https://buy.stripe.com/3cIfZj2zm5KE2QLgoX8bS0n",
+  p3: "https://buy.stripe.com/fZu00l7TG1uoezt5Kj8bS0o",
+  p4: "https://buy.stripe.com/7sY14pfm8c929f9fkT8bS0p",
+  p5: "https://buy.stripe.com/9B65kF4Hua0UdvpfkT8bS0q",
+};
